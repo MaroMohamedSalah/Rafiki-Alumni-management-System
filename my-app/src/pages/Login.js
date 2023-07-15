9f4976dbbfd3f906835516a61f4803d309049b41
MaroMohamedSalah <91625271+MaroMohamedSalah@users.noreply.github.com>
2023-07-15 23:17:22 +0300

    refine Login

--- my-app/src/pages/Login.js
+++ my-app/src/pages/Login.js
@@ -1,32 +1,28 @@
 import { Link, useNavigate } from "react-router-dom";
-import ProgressLine from "../components/ProgressLine";
+import { useState } from "react";
+import Swal from "sweetalert2";
 import Backbtn from "../components/Backbtn";
 import loginImg from "../imgs/login img.svg";
-import "./Auth.css";
 import icon1 from "../imgs/sign up 3.svg";
 import icon2 from "../imgs/sign up 5.svg";
 import show from "../imgs/show password.svg";
 import hide from "../imgs/hide password.svg";
-import { useState } from "react";
-import Swal from "sweetalert2";
-// import jwt from "jsonwebtoken";
+import "./Auth.css";
 
 const Login = () => {
 	const [password, setPassword] = useState("");
 	const [showPassword, setShowPassword] = useState(false);
 	const [usernameError, setUsernameError] = useState("");
 	const [passwordError, setPasswordError] = useState("");
-	const navigate = useNavigate(); // Get navigate function from react-router-dom
+	const navigate = useNavigate();
 
-	// check if any input is empty
 	const validateInputsValues = (inputs) => {
 		let error = false;
 
 		Array.from(inputs).forEach((input) => {
 			if (input[1].length === 0) {
 				error = true;
-				console.error(`Error: ${input[0]} is required`); // Log error message for empty input
-				// Set error message for empty input using state setter functions
+				console.error(`Error: ${input[0]} is required`);
 				switch (input[0]) {
 					case "username":
 						setUsernameError(`${input[0]} is required`);
@@ -39,20 +35,19 @@ const Login = () => {
 				}
 			}
 		});
-		return error; // Return true indicating if any input is empty
+		return error;
 	};
 
 	const handelSubmit = async (e) => {
+		e.preventDefault();
 		const submitBtn = document.querySelector(".Auth form .submit button");
 		const form = document.querySelector(".Auth form");
-		e.preventDefault();
 
-		// Form data validation logic here
 		const formData = new FormData(form);
 		let anyInputIsEmpty = validateInputsValues(formData);
-		if (!usernameError && !passwordError && anyInputIsEmpty !== true) {
+		if (!usernameError && !passwordError && !anyInputIsEmpty) {
 			submitBtn.setAttribute("disabled", true);
-			// If no errors exist, send form data to API
+
 			try {
 				const response = await fetch(
 					"https://alumni-system-backend.azurewebsites.net/api/users/login",
@@ -67,22 +62,21 @@ const Login = () => {
 						},
 					}
 				);
+
 				const data = await response.json();
 				const cookies = response.headers.get("Cookie");
 				console.log("Cookies:", cookies);
-				// Handle response from API here
-				// Example: Check if response indicates success or error
+
 				submitBtn.removeAttribute("disabled");
 				if (response.status === 200) {
-					// record the user id in localStorage
 					localStorage.setItem("UserID", data.user_id);
-					// Success
+
 					Swal.fire({
 						title: "Success!",
 						text: `You Logged in successfully, as ${data.actor}!`,
 						icon: "success",
 					});
-					// Redirect to Actor profile page
+
 					switch (data.actor) {
 						case "Alumni":
 							navigate("/alumniProfile");
@@ -96,24 +90,20 @@ const Login = () => {
 							navigate("/studentProfile");
 							displayNotification(data.username);
 							break;
-
 						case "Admin":
 							navigate("/adminProfile");
 							displayNotification(data.username);
 							break;
-
 						case "Professors":
 							navigate("/professorProfile");
 							displayNotification(data.username);
 							break;
-
 						default:
 							break;
 					}
 
-					console.log("Logged in successful!", data);
+					console.log("Logged in successfully!", data);
 				} else {
-					// Error
 					console.error("Error: " + data.message);
 					setPasswordError("Password or Username may be incorrect");
 					setUsernameError("Password or Username may be incorrect");
@@ -123,9 +113,8 @@ const Login = () => {
 			}
 		}
 	};
-	console.log("v-3");
+
 	const displayNotification = (username) => {
-		// Display welcome notification with vibration
 		if ("Notification" in window && Notification.permission === "granted") {
 			const notification = new Notification("Welcome Back!", {
 				body: `Hey there, ${username}! Thanks for logging in. Check out your profile.`,
@@ -143,12 +132,12 @@ const Login = () => {
 		<div className="Auth Login">
 			<div className="container-fluid">
 				<Backbtn
-					btnColor={"var(--login-color-2)"}
-					btnSize={"25px"}
-					btnTop={"10px"}
-					btnColorMobile={"var(--Alumni-color)"}
-					btnSizeMobile={"15px"}
-					btnTopMobile={"10px"}
+					btnColor="var(--login-color-2)"
+					btnSize="25px"
+					btnTop="10px"
+					btnColorMobile="var(--Alumni-color)"
+					btnSizeMobile="15px"
+					btnTopMobile="10px"
 				/>
 				<div className="row">
 					<div className="col-12 col-md-6 d-none d-md-flex justify-content-between align-items-center flex-column">
@@ -156,7 +145,7 @@ const Login = () => {
 						<div className="toSignup d-flex justify-content-center align-items-center w-100 mb-2">
 							<h5 className="m-0">Not A Member ? </h5>
 							<span>
-								<Link to={"/roleSelection"}>
+								<Link to="/roleSelection">
 									<button className="btn px-4 py-1 fs-6 text mx-4">
 										Signup
 									</button>
@@ -186,13 +175,11 @@ const Login = () => {
 											aria-describedby="basic-addon1"
 											onChange={(e) => {
 												const regex = /^[a-zA-Z0-9_]{3,20}$/;
-												if (!regex.test(e.target.value)) {
-													setUsernameError(
-														"Username must be 3-20 characters long and can only contain letters, numbers, and underscores."
-													);
-												} else {
-													setUsernameError("");
-												}
+												setUsernameError(
+													regex.test(e.target.value)
+														? ""
+														: "Username must be 3-20 characters long and can only contain letters, numbers, and underscores."
+												);
 											}}
 										/>
 									</div>
@@ -207,19 +194,21 @@ const Login = () => {
 										<input
 											name="password"
 											id="password"
-											type={showPassword === true ? "text" : "password"}
+											type={showPassword ? "text" : "password"}
 											className="form-control"
 											placeholder="Password"
 											aria-label="Password"
 											aria-describedby="basic-addon1"
 											onChange={(e) => {
-												e.target.value < 6
-													? setPasswordError("Invalid Password length")
-													: setPasswordError("");
+												setPasswordError(
+													e.target.value.length < 6
+														? "Invalid Password length"
+														: ""
+												);
 											}}
 										/>
 										<span className="input-group-text" id="basic-addon1">
-											{showPassword === true ? (
+											{showPassword ? (
 												<img
 													src={hide}
 													alt=""
@@ -243,7 +232,7 @@ const Login = () => {
 								</div>
 								<h5 className="text-center forgetPass">
 									forget password?{" "}
-									<Link className="d-block d-md-inline" to={"/resetPass"}>
+									<Link className="d-block d-md-inline" to="/resetPass">
 										Reset Password
 									</Link>{" "}
 								</h5>
@@ -252,8 +241,7 @@ const Login = () => {
 								</div>
 								<div className="toSignup d-flex d-md-none justify-content-evenly align-items-center w-100 mb-2">
 									<h5 className="m-0">Not A Member ? </h5>
-
-									<Link to={"/roleSelection"}>
+									<Link to="/roleSelection">
 										<button className="btn px-4 py-1 fs-6 text m-0">
 											Signup
 										</button>
@@ -267,4 +255,5 @@ const Login = () => {
 		</div>
 	);
 };
+
 export default Login;
