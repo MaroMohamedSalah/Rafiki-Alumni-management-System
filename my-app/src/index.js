b4d33da091de77ab1bdd47a8ef4fd116f227fd42
MaroMohamedSalah <91625271+MaroMohamedSalah@users.noreply.github.com>
2023-10-06 19:40:51 +0300

    add Google

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -6,6 +6,21 @@ import reportWebVitals from "./reportWebVitals";
 import { Provider } from "react-redux";
 import store from "./redux/store";
 
+// import { initializeApp } from "firebase/app";
+
+// const firebaseConfig = {
+// 	apiKey: "AIzaSyAASt292PDZWEGmcDuuLpVbH4566YBSM9Y",
+// 	authDomain: "alumni-management-system-52f7e.firebaseapp.com",
+// 	projectId: "alumni-management-system-52f7e",
+// 	storageBucket: "alumni-management-system-52f7e.appspot.com",
+// 	messagingSenderId: "148006699798",
+// 	appId: "1:148006699798:web:5f54d140ad754a3a000637",
+// 	measurementId: "G-2Q94LWLZNE",
+// };
+
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
+
 if ("serviceWorker" in navigator) {
 	window.addEventListener("load", () => {
 		navigator.serviceWorker
