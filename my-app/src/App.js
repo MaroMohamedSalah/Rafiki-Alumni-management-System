a7dd6d7e08028b4947bcc311957285a009d27b3c
MaroMohamedSalah <91625271+MaroMohamedSalah@users.noreply.github.com>
2024-02-24 17:38:44 +0200

    Revert "small edits"
    This reverts commit 4e2114042e6e64c5ab8c14a3ffc9df1c7d6c362c.

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -26,66 +26,66 @@ import GetStartedAr from "./pages/GetStartedAr";
 import Signup from "./pages/Signup";
 import ComingSoon from "./pages/ComingSoon";
 import NotFound from "./pages/NotFound";
-import UploadMaterials from "./pages/UploadMaterials/UploadMaterials";
-import UploadMaterialsPopUp from "./components/UploadMaterialsPopUp";
+import UploadMatrials from "./pages/UploadMatrials/UploadMatrials";
 
 function App() {
-	return (
-		<div className="App">
-			<LocalizationProvider dateAdapter={AdapterDayjs}>
-				<BrowserRouter>
-					<Routes>
-						{/* Public Routes */}
-						<Route path="/" element={<LandingPage />} />
-						<Route path="/roleSelection" element={<RoleSelection />} />
-						<Route path="/login" element={<LoginLayout />} />
-						<Route path="/resetPass" element={<ResetPassLayout />}>
-							<Route index element={<ResetPassEmail />} />
-							<Route path="checkYourEmail" element={<CheckYourEmail />} />
-							<Route
-								path="createNewPassword/:token"
-								element={<CreateNewPassword />}
-							/>
-							<Route
-								path="createNewPasswordSuccess"
-								element={<CreateNewPassSuccess />}
-							/>
-						</Route>
-						{/* Private Routes */}
-						{/* Private routes are routes that require the user to be authenticated or
+  return (
+    <div className="App">
+      <LocalizationProvider dateAdapter={AdapterDayjs}>
+        <BrowserRouter>
+          <Routes>
+            {/* Public Routes */}
+            <Route path="/" element={<LandingPage />} />
+            <Route path="/roleSelection" element={<RoleSelection />} />
+            <Route path="/login" element={<LoginLayout />} />
+            <Route path="/resetPass" element={<ResetPassLayout />}>
+              <Route index element={<ResetPassEmail />} />
+              <Route path="checkYourEmail" element={<CheckYourEmail />} />
+              <Route
+                path="createNewPassword/:token"
+                element={<CreateNewPassword />}
+              />
+              <Route
+                path="createNewPasswordSuccess"
+                element={<CreateNewPassSuccess />}
+              />
+            </Route>
+            {/* Private Routes */}
+            {/* Private routes are routes that require the user to be authenticated or
 					logged in to access them. */}
-						<Route path="/dashboard" element={<DashboardLayout />}>
-							{/* <Route index element={<h1>User will see this first</h1>} /> */}
-							<Route path="postJob" element={<PostJobs />} />
-							<Route path="postIntern" element={<PostIntern />} />
-							<Route path="applyJob" element={<ApplyJobLayout />}>
-								<Route index path=":jobId" element={<JobDetail />} />
-							</Route>
-							<Route
-								path="jobsApplications"
-								element={<ComingSoon date={"01/11/2023"} />}
-							/>
-						</Route>
-						<Route path="/getStarted" element={<GetStarted />} />
-						<Route path="/getStartedAr" element={<GetStartedAr />} />
-						{/* Signup Routes */}
-						<Route path="/alumniSignup" element={<AlumniSignup />} />
-						<Route path="/signup/:actor" element={<Signup />} />
-						<Route path="/studentSignup" element={<CurrantStudentSignup />} />
-						<Route path="/hrSignup" element={<HRSignup />} />
-						{/* Profile Routes */}
-						<Route path="/profile" element={<ProfileLayout />}>
-							<Route index element={<ProfileContent />} />
-							<Route path="addSkills" element={<AddSkills />} />
-						</Route>
-						{/* Not Found Route */}
-						<Route path="*" element={<NotFound />} />
-						<Route path="upload-material" element={<UploadMaterials />} />
-					</Routes>
-				</BrowserRouter>
-			</LocalizationProvider>
-		</div>
-	);
+            <Route path="/dashboard" element={<DashboardLayout />}>
+              {/* <Route index element={<h1>User will see this first</h1>} /> */}
+              <Route path="postJob" element={<PostJobs />} />
+              <Route path="postIntern" element={<PostIntern />} />
+              <Route path="applyJob" element={<ApplyJobLayout />}>
+                <Route index path=":jobId" element={<JobDetail />} />
+              </Route>
+              <Route
+                path="jobsApplications"
+                element={<ComingSoon date={"01/11/2023"} />}
+              />
+            </Route>
+            <Route path="/getStarted" element={<GetStarted />} />
+            <Route path="/getStartedAr" element={<GetStartedAr />} />
+            {/* Signup Routes */}
+            <Route path="/alumniSignup" element={<AlumniSignup />} />
+            <Route path="/signup/:actor" element={<Signup />} />
+            <Route path="/studentSignup" element={<CurrantStudentSignup />} />
+            <Route path="/hrSignup" element={<HRSignup />} />
+            {/* Profile Routes */}
+            {/* <Route path="/profile" element={<Profile />} /> */}
+            <Route path="/profile" element={<ProfileLayout />}>
+              <Route index element={<ProfileContent />} />
+              <Route path="addSkills" element={<AddSkills />} />
+            </Route>
+            {/* Not Found Route */}
+            <Route path="*" element={<NotFound />} />
+            <Route path="upload-material" element={<UploadMatrials />} />
+          </Routes>
+        </BrowserRouter>
+      </LocalizationProvider>
+    </div>
+  );
 }
 
 export default App;
