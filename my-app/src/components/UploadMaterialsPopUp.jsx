4e2114042e6e64c5ab8c14a3ffc9df1c7d6c362c
MaroMohamedSalah <91625271+MaroMohamedSalah@users.noreply.github.com>
2024-02-24 14:21:23 +0200

    small edits

--- my-app/src/components/UploadMaterialsPopUp.jsx
+++ my-app/src/components/UploadMaterialsPopUp.jsx
@@ -1,67 +1,74 @@
-import * as React from 'react';
-import Button from '@mui/material/Button';
-import Dialog from '@mui/material/Dialog';
-import DialogActions from '@mui/material/DialogActions';
-import DialogContent from '@mui/material/DialogContent';
-import DialogContentText from '@mui/material/DialogContentText';
-import DialogTitle from '@mui/material/DialogTitle';
-import Lottie from 'lottie-react'
-import animation from '../../animations/successfully done.json'
-import style from './UploadMaterialsPopUp.module.css'
-import { Link } from 'react-router-dom';
+import * as React from "react";
+import Button from "@mui/material/Button";
+import Dialog from "@mui/material/Dialog";
+import DialogActions from "@mui/material/DialogActions";
+import DialogContent from "@mui/material/DialogContent";
+import DialogContentText from "@mui/material/DialogContentText";
+import DialogTitle from "@mui/material/DialogTitle";
+import Lottie from "lottie-react";
+import animation from "../animations/successfully done.json";
+import style from "./UploadMaterialsPopUp.module.css";
+import { Link } from "react-router-dom";
 
 export default function UploadMaterialsPopUp() {
+	const [open, setOpen] = React.useState(false);
 
+	const handleClickOpen = () => {
+		setOpen(true);
+	};
 
-    const [open, setOpen] = React.useState(false);
+	const handleClose = () => {
+		setOpen(false);
+	};
 
-    const handleClickOpen = () => {
-        setOpen(true);
-    };
+	return (
+		<React.Fragment>
+			<Button
+				variant="contained"
+				onClick={handleClickOpen}
+				className="d-block m-auto mt-5"
+			>
+				Submit
+			</Button>
+			<Dialog
+				open={open}
+				onClose={handleClose}
+				aria-labelledby="alert-dialog-title"
+				aria-describedby="alert-dialog-description"
+				className="m-auto col-lg-6 col-md-7 col-sm-12"
+			>
+				<DialogTitle id="alert-dialog-title" className="p-1 pt-4 pb-4">
+					<Lottie
+						loop={false}
+						animationData={animation}
+						className="w-25 d-block m-auto"
+					></Lottie>
+				</DialogTitle>
+				<DialogContent>
+					<DialogContentText
+						id="alert-dialog-description"
+						className={style.editContent + " text-center"}
+					>
+						<h4>Your Materiel Is Under Review</h4>
+						<h5>Thank you for your participation</h5>
+					</DialogContentText>
+				</DialogContent>
+				<DialogActions>
+					<Button
+						className={style.btnConfig + " m-auto mb-4 w-50 p-3 text-white"}
+					>
+						Home page
+					</Button>
 
-    const handleClose = () => {
-        setOpen(false);
-    };
-
-    return (
-        <React.Fragment>
-            <Button variant="contained" onClick={handleClickOpen} className='d-block m-auto mt-5'>
-                Submit
-            </Button>
-            <Dialog
-                open={open}
-                onClose={handleClose}
-                aria-labelledby="alert-dialog-title"
-                aria-describedby="alert-dialog-description"
-                className='m-auto col-lg-6 col-md-7 col-sm-12'
-            >
-
-                <DialogTitle id="alert-dialog-title" className='p-1 pt-4 pb-4'>
-                    <Lottie loop={false} animationData={animation} className='w-25 d-block m-auto'></Lottie>
-                </DialogTitle>
-                <DialogContent>
-                    <DialogContentText id="alert-dialog-description" className={style.editContent + ' text-center'}>
-                        <h4>Your Materiel Is under review</h4>
-                        <h5>Thank you for your participation</h5>
-                    </DialogContentText>
-                </DialogContent>
-                <DialogActions>
-
-                    <Button className={style.btnConfig + " m-auto mb-4 w-50 p-3 text-white"}>
-                            Home page
-                    </Button>
-
-                    {/* <Link
+					{/* <Link
                         to="./"
                         variant="contained"
                         className={style.btnConfig + " m-auto mb-4 w-50 p-3 text-white"}
                     >
                         Home page
                     </Link> */}
-
-
-                </DialogActions>
-            </Dialog>
-        </React.Fragment>
-    );
+				</DialogActions>
+			</Dialog>
+		</React.Fragment>
+	);
 }
