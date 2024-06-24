8f9a8d27cc25efef063e399e76d13f211b56bec0
MaroMohamedSalah <91625271+MaroMohamedSalah@users.noreply.github.com>
2024-06-24 20:54:03 +0300

    Revert "update base backend url"
    This reverts commit e09c0340eff78f73eac1f89a035e85e7c9d70a45.

--- my-app/src/utils/baseBackendUrl.js
+++ my-app/src/utils/baseBackendUrl.js
@@ -1,2 +1,1 @@
-// export const baseBackendUrl = "https://rafiki-backend1.azurewebsites.net/api";
-export const baseBackendUrl = "http://localhost:3008/";
+export const baseBackendUrl = "https://rafiki-backend1.azurewebsites.net/api";
