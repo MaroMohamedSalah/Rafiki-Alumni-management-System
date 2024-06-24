e09c0340eff78f73eac1f89a035e85e7c9d70a45
MaroMohamedSalah <91625271+MaroMohamedSalah@users.noreply.github.com>
2024-06-24 20:49:31 +0300

    update base backend url

--- my-app/src/utils/baseBackendUrl.js
+++ my-app/src/utils/baseBackendUrl.js
@@ -1,1 +1,2 @@
-export const baseBackendUrl = "https://rafiki-backend1.azurewebsites.net/api";
+// export const baseBackendUrl = "https://rafiki-backend1.azurewebsites.net/api";
+export const baseBackendUrl = "http://localhost:3008/";
